4a9d41ee839e9de5c4d35fe1794873122fdfa1eb
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-10-14 16:01:55 +0200

    introduce databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,23 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false
+  serverCreationStatus = 'No server was created!'
+
+  constructor() {
+    setTimeout(() => this.allowNewServer = true, 2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!'
   }
 
 }

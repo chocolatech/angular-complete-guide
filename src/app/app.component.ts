7928c780a51c741b58fd7db85ce6436d3c7a9fea
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-10-15 16:36:11 +0200

    submit assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .whiteFont {
+      color: white
+    }
+  `]
 })
 export class AppComponent {
+  toggled = false
+  clicks = []
+  id = 0
 
+  onButtonToggle() {
+    this.toggled = !this.toggled
+
+    // this.clicks.push(this.clicks.length + 1)
+    this.clicks.push(new Date())
+  }
 }

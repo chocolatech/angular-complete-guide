a77fc674ec478a9c80935cc2860e892404445f86
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-10-15 15:55:27 +0200

    introduce directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = 'No server was created!'
   serverName = 'TestServer'
+  serverCreated = false
+  servers = ['testserver', 'Testserver 2']
 
   constructor() {
     setTimeout(() => this.allowNewServer = true, 2000)
@@ -21,6 +23,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
   }
 

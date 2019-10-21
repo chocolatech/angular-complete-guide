50465b87fb1b8858d21534dae5523ddd028fa8a2
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-10-21 16:44:33 +0200

    add communication between components

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>()
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddServer(nameInput:HTMLInputElement) {
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value })
+  }
+
+  onAddBlueprint(nameInput:HTMLInputElement) {
+    this.blueprintCreated.emit({ serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value })
+  }
+
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, ViewEncapsulation, ViewChild, ElementRef, ContentChild } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native, ShadowDom
+})
+export class ServerElementComponent implements OnInit {
+  @Input('srvElement') element: { type: string, name: string, content: string }
+  @ViewChild('heading', {static: true}) header: ElementRef
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
